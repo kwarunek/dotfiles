9668abde1b742248782318f4e0e185736a3967fd
kwarunek <kAlmAcetA@users.noreply.github.com>
2014-05-08 00:32:34 +0200

    rm unused, added rvm, nvm

--- .vim/install.sh
+++ .vim/install.sh
@@ -2,20 +2,10 @@
 
 
 sudo pip install flake8
-mkdir -p ~/.vim/autoload ~/.vim/bundle ~/.vim/tags  ~/.vim/colors; \
-curl -Sso ~/.vim/autoload/pathogen.vim \
-    https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+
+#curl -Sso ~/.vim/autoload/pathogen.vim \
+#    https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+curl -sSL https://get.rvm.io | bash -s stable --ruby
+curl https://raw.githubusercontent.com/creationix/nvm/v0.6.1/install.sh | sh
 ctags -R -f ~/.vim/tags/python.ctags /usr/lib/python2.7
-cd ~/.vim/bundle
-git clone https://github.com/nvie/vim-flake8.git
-git clone https://github.com/scrooloose/nerdtree.git
-git clone https://github.com/scrooloose/nerdcommenter.git
-git clone https://github.com/bling/vim-airline.git
-cd /tmp
-git clone https://github.com/jelera/vim-gummybears-colorscheme.git vimgummybears
-cp -r vimgummybears/colors/* ~/.vim/colors
-git clone https://github.com/tpope/vim-vividchalk.git vividchalk
-cp -r vividchalk/colors/* ~/.vim/colors
-# sudo npm install -g js-yaml
-# sudo npm install -g jsonlint
 sudo npm install -g jshint
