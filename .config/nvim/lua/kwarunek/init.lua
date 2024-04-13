38e896efc4614e9c85250dd1a6285969166d564d
Krzysztof Warunek <krzysztof@warunek.net>
2024-04-13 14:09:02 +0200

    telescope

--- .config/nvim/lua/kwarunek/init.lua
+++ .config/nvim/lua/kwarunek/init.lua
@@ -64,6 +64,48 @@ require("lazy").setup({
             highlight_visible = true,
             color_icons = true
         }
+    },
+    {
+        'nvim-telescope/telescope.nvim', tag = '0.1.6',
+        dependencies = { 'nvim-lua/plenary.nvim' },
+        config = function()
+            local actions = require("telescope.actions")
+            local action_layout = require("telescope.actions.layout")
+            require('telescope').setup({
+                defaults = {
+                    mappings = {
+                        n = {
+                            ["<M-p>"] = action_layout.toggle_preview
+                        },
+                        i = {
+                            ["<esc>"] = actions.close,
+                            ["<M-p>"] = action_layout.toggle_preview
+                        },
+                    },
+                    results_title = false,
+                    prompt_title = false,
+                    preview ={
+                        hide_on_startup = true,
+                    },
+                },
+            })
+
+            local builtin = require('telescope.builtin')
+            vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+            vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+            vim.keymap.set('n', '<leader>pws', function()
+                local word = vim.fn.expand("<cword>")
+                builtin.grep_string({ search = word })
+            end)
+            vim.keymap.set('n', '<leader>pWs', function()
+                local word = vim.fn.expand("<cWORD>")
+                builtin.grep_string({ search = word })
+            end)
+            vim.keymap.set('n', '<leader>ps', function()
+                builtin.grep_string({ search = vim.fn.input("Grep > ") })
+            end)
+            vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+        end
     }
 })
 
