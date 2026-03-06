edb67c0e9fb5c1e4c5462c6e575e97fe5ea23daa
Krzysztof Warunek <krzysztof@warunek.net>
2026-03-06 13:47:07 +0100

    nvim plugins up

--- .config/nvim/lua/kwarunek/lazy/treesitter.lua
+++ .config/nvim/lua/kwarunek/lazy/treesitter.lua
@@ -2,7 +2,7 @@ return    {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function ()
-        local configs = require("nvim-treesitter.configs")
+        local configs = require("nvim-treesitter.config")
 
         configs.setup({
             ensure_installed = { "asm", "awk", "bash", "c", "capnp", "cmake", "cpp", "css", "csv", "d", "diff", "dockerfile", "doxygen", "gdscript", "go", "gosum", "gomod", "groovy", "hcl", "html", "ini", "java", "json", "jsdoc", "lua", "make", "markdown", "objdump", "pascal", "perl", "php", "puppet", "python", "rego", "rst", "ruby", "rust", "sql", "strace", "templ", "terraform", "toml", "typescript", "vim", "vimdoc", "xml", "yaml", "zig"},
